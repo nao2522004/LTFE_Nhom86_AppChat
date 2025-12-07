105271512db5cd9b760530ddb0cbfaf3aedc58d4
HQLuat <22130157@st.hcmuaf.edu.vn>
2025-12-07 15:42:54 +0700

    route configuration for LoginPage and RegisterPage

---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RegisterForm from '../features/auth/components/RegisterForm';
+
+const RegisterPage: React.FC = () => {
+    return <RegisterForm />;
+};
+
+export default RegisterPage;
